389e9ad6bf989648944a66532ee33803c1d6c09e
Alabi Olalekan Emmanuel <alabilekanemmanuel@gmail.com>
2022-06-27 23:14:27 +0100

    Add files via upload

--- static-job-listings-master/script.js
+++ static-job-listings-master/script.js
@@ -1,110 +1,107 @@
-const searchBar = document.getElementById('search');
+const searchBar = document.getElementById("search");
 let cardContainer = document.getElementById("card-container");
 let jobLists = new Array();
 
-searchBar.addEventListener('keyup', (e) => {
-    let searchString = e.target.value.toLowerCase();
+searchBar.addEventListener("keyup", (e) => {
+  let searchString = e.target.value.toLowerCase();
 
-   const filteredJobs = jobLists.filter(job => {
-
+  const filteredJobs = jobLists.filter((job) => {
     const jobLangString = job.languages.toString().toLowerCase();
     const jobToolString = job.tools.toString().toLowerCase();
 
-     return (
-       job.role.toLowerCase().includes(searchString) ||
-       job.level.toLowerCase().includes(searchString) ||
-       job.location.toLowerCase().includes(searchString) ||
-       jobLangString.includes(searchString) ||
-       jobToolString.includes(searchString)
-     ); 
+    return (
+      job.role.toLowerCase().includes(searchString) ||
+      job.level.toLowerCase().includes(searchString) ||
+      job.location.toLowerCase().includes(searchString) ||
+      jobLangString.includes(searchString) ||
+      jobToolString.includes(searchString)
+    );
+  });
 
-   });
-
-    //Displaying only jobs that matches the search text (filtered)
-    displayJobLists(filteredJobs);
+  //Displaying only jobs that matches the search text (filtered)
+  displayJobLists(filteredJobs);
 });
 
-
 async function loadJobLists() {
-
-try {
+  try {
     const response = await fetch("data.json");
     jobLists = await response.json();
 
     // Displaying the data from the API
-    displayJobLists(jobLists); 
-   
-} catch(err) {
-        console.error(`AN ERROR OCCURRED: ${err}`);
-      };
-      
-    //   const url = 'data.json';
+    displayJobLists(jobLists);
+  } catch (err) {
+    console.error(`AN ERROR OCCURRED: ${err}`);
+  }
 
-    //   let response = await fetch(url);
-    //   let data = await response.json();
+  //   const url = 'data.json';
 
-    //    displayJobLists(data)    
-};
-   
- loadJobLists();//Loading the API data by calling the function.
+  //   let response = await fetch(url);
+  //   let data = await response.json();
+
+  //    displayJobLists(data)
+}
+
+loadJobLists(); //Loading the API data by calling the function.
 
 function displayJobLists(jobs) {
+  const htmlString = jobs
+    .map((job) => {
+      if (job.new === true) {
+        job.new = "New!";
+      } else {
+        job.new = "";
+      }
 
-    const htmlString = jobs.map(job => {
+      if (job.featured === true) {
+        job.featured = "Featured";
+      } else {
+        job.featured = "";
+      }
 
+      return `
 
-        if(job.new === true){
-            job.new = 'New'
-        };
+ <div class="cards">
+          <div class="details">
         
-        if (job.new === false) {
-            job.new = '';
-
-        };
-
-
-        return `
-
-        <div class="cards">
-        <div class="details">
-
-          <div class="logo">
-            <img src="${job.logo}" alt="${job.company}">
+            <div class="logo">
+              <img src="${job.logo}" alt="${job.company}">
+            </div>
+        
+            <div class="flex-desk">
+              <div class="features">
+                <h4>${job.company}</h4>
+                <div class="time">
+                <p class="new">${job.new}</p>
+                <p>${job.featured}</p>
+                </div>
+              </div>
+              <h4>${job.position}</h4>
+              <div class="preferences">
+                <ul>
+                  <li style="list-style: none;">${job.postedAt}</li>
+                  <li>${job.contract}</li>
+                  <li>${job.location}</li>
+                </ul>
+              </div>
+            </div>
+        
           </div>
-
-          <div class="features">
-            <h4>${job.company}</h4>
-            <p class="new">${job.new}</p>
-            <p>${job.featured}</p>
+        
+          <div class="roles">
+            <p>${job.role}</p>
+            <p>${job.level}</p>
+            <p>${job.languages[0]}</p>
+            <p>${job.languages[1]}</p>
+            <p>${job.languages[2]}</p>
+            <p>${job.tools[0]}</p>
+            <p>${job.tools[1]}</p>
           </div>
-
-          <h4>${job.position}</h4>
-
-          <div class="preferences">
-            <ul>
-              <li style="list-style: none;">${job.postedAt}</li>
-              <li>${job.contract}</li>
-              <li>${job.location}</li>
-            </ul>
-          </div>
-
-        </div>
-
-        <div class="roles">
-          <p>${job.role}</p>
-          <p>${job.level}</p>
-          <p>${job.languages[0]}</p>
-          <p>${job.languages[1]}</p>
-          <p>${job.languages[2]}</p>
-          <p>${job.tools[0]}</p>
-          <p>${job.tools[1]}</p>
-        </div>
-
-      </div>
+        
+ </div>
         
         `;
     })
-    
+    .join("");
 
-    cardContainer.innerHTML = htmlString;
-};
+  cardContainer.innerHTML = htmlString;
+}
